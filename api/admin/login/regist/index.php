8cc06065a4bc7b4ce708959e29e1a11056cf962f
49372s <kyouyouidsiyou@gmail.com>
2023-06-24 21:23:18 +0900

    add: 互換性モード

--- api/admin/login/regist/index.php
+++ api/admin/login/regist/index.php
@@ -9,12 +9,21 @@ $pdo = cdb();
 $sql = "SELECT * from account";
 $res = $pdo->query($sql);
 
-if($res->rowCount() == MAX_ADMINISTRATOR && MAX_ADMINISTRATOR!=0){
-    APIResponse(false,"You have reached the maximum number of administrators. The system has rejected your request to create an administrator.<br>For more information, contact your system administrator or developer.");
+$count = 0;
+if(MYSQL_COMPATIBLE == 0){
+    if($res->rowCount() == MAX_ADMINISTRATOR && MAX_ADMINISTRATOR!=0){
+        APIResponse(false,"You have reached the maximum number of administrators. The system has rejected your request to create an administrator.<br>For more information, contact your system administrator or developer.");
+    }
 }
 foreach($res as $val){
     if($val[0]==$_POST['id']){
         APIResponse(false,$_POST['id']." is already exists.");
+    }
+    if(MAX_ADMINISTRATOR!=0){
+        $count++;
+        if($count==MAX_ADMINISTRATOR){
+            APIResponse(false,"You have reached the maximum number of administrators. The system has rejected your request to create an administrator.<br>For more information, contact your system administrator or developer.");
+        }
     }
 }
 
